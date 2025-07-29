ce08b9c4680fbd4fea2a521381b4b0ac2d893459
Kapavarapu <durgadevi.kapavarapu@accenture.com>
2025-07-29 22:27:45 +0530

    committing docker

--- docker-installation.sh
+++ docker-installation.sh
@@ -10,3 +10,4 @@ sudo dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker
 sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 sudo systemctl start docker
 sudo systemctl enable docker
+sudo usermod -aG docker ec2-user
