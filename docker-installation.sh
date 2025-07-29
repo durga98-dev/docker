2633778e223043d460eaf823d04a1a53ab938cbb
Kapavarapu <durgadevi.kapavarapu@accenture.com>
2025-07-29 22:15:39 +0530

    committing docker

--- docker-installation.sh
+++ docker-installation.sh
@@ -1,3 +1,10 @@
+lsblk
+sudo growpart /dev/nvme0n1 4
+sudo lvextend -l +50%FREE /dev/RootVG/rootVol
+sudo lvextend -l +50%FREE /dev/RootVG/varVol
+sudo xfs_growfs /
+sudo xfs_growfs /var
+
 sudo dnf -y install dnf-plugins-core
 sudo dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
 sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
--- ec2.tf
+++ ec2.tf
@@ -2,6 +2,11 @@ resource "aws_instance" "docker-server"{
     ami = "ami-09c813fb71547fc4f"
     vpc_security_group_ids = [aws_security_group.docker_sg.id] 
     instance_type = local.instance_type
+    root_block_device {
+        volume_size = 50
+        volume_type = "gp3"
+  }
+  user_data = file("docker-installation.sh")
     tags = {
         Name = "Docker-demo"
     }
@@ -21,14 +26,14 @@ resource "aws_security_group" "docker_sg" {
         from_port = 22
         to_port = 22
         protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
+        cidr_blocks = [ "0.0.0.0/0" ]
     }
 
     ingress {
-        from_port = 80
-        to_port = 80
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = [ "0.0.0.0/0" ]
     }
 
     tags = {
